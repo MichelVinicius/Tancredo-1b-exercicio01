13568972e3f392cecbb8d687a32d846c1d2b59ca
Michel Vinicius <141455136+MichelVinicius@users.noreply.github.com>
2023-08-11 17:41:20 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600, 600);
+    background("orange");
+
+}
+
+function draw() {
+  
+  fill("red");
+  stroke("yellow");
+  
+  if(mouseIsPressed){
+  rect (mouseX,mouseY,35,35) 
+}
+
+ 
+}
